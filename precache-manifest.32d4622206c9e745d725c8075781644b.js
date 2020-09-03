d3d1ff8c730324e714ff5f6afec91ab919364567
William Gyasi <kwabenagyasi63@gmail.com>
2020-09-03 13:59:49 +0000

    Updates

---
+++ precache-manifest.32d4622206c9e745d725c8075781644b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ef1dfa6f01109c4d1bcf86f9a6b0df",
+    "url": "/React-Voting-App/index.html"
+  },
+  {
+    "revision": "92d4b7167dd1d32b5038",
+    "url": "/React-Voting-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6bb99acc75197ae6070d",
+    "url": "/React-Voting-App/static/js/2.0e0d494f.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/React-Voting-App/static/js/2.0e0d494f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d4b7167dd1d32b5038",
+    "url": "/React-Voting-App/static/js/main.6280ad63.chunk.js"
+  },
+  {
+    "revision": "af15f2d3d633747c4292",
+    "url": "/React-Voting-App/static/js/runtime-main.6c48f429.js"
+  },
+  {
+    "revision": "6596e7d65ffc2ed8d6edea02db9191be",
+    "url": "/React-Voting-App/static/media/candidate-face.6596e7d6.jpg"
+  }
+]);

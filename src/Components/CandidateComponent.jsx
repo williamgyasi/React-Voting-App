f73ba04d7cae0a9372199e1416c68bbd917366b0
William Gyasi <kwabenagyasi63@gmail.com>
2020-09-06 23:53:56 +0000

    Carousel Movement

--- src/Components/CandidateComponent.jsx
+++ src/Components/CandidateComponent.jsx
@@ -1,29 +1,25 @@
 import React from 'react'
 import {Container,Button,TextField} from '@material-ui/core'
 import CheckIcon from '@material-ui/icons/Check';
-const Candidate =({name,position})=>{
+const Candidate =({candidateName,position,uri,state})=>{
     return(
         <div
         style={{
             width:270,
             height:300,
-            borderRadius:10
+            borderRadius:10,
+            padding:10,
         }}>
         <img
         style={{
             width:"100%",
             height:"90%",
         }}
-        src={require('../Assets/candidate-face.jpg')}
+        src={uri}
         alt={"Face"} />
-        <h3 style={{textAlign:"center",marginTop:10}}>William Gyasi</h3>
-        <h5 style={{textAlign:"center"}}>Head of ushers</h5>
-        <Button variant="contained" 
-        style={{color:"white",backgroundColor:"green",width:"100%",marginTop:10,}}
-        >
-            <CheckIcon style={{marginRight:10}} />
-            Vote
-        </Button>            
+        <h2 style={{textAlign:"center",marginTop:5,marginBlockEnd:0,fontSize:30}}>{candidateName}</h2>
+        <h3 style={{textAlign:"center",marginTop:0,color:"#9ea2a8"}}>{position}</h3>
+                  
         </div>
 
     )
--- src/Pages/UserHomePage.js
+++ src/Pages/UserHomePage.js
@@ -1,30 +1,93 @@
 import React,{useContext,useState,useEffect} from 'react'
 import UserHeaderBar from '../Components/UserHeaderBar'
 import {firebaseAuth} from '../ContextProviders/UserAuthProvider';
+import { List, Card, Space } from 'antd';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import {Button} from '@material-ui/core'
 
 import {firestoreMethods} from '../Firebase/firestoreMethods'
+
+import CandidateComponent from '../Components/CandidateComponent'
 
 
 const Homepage =()=>{
     document.title="Voting"
-    const{email} = useContext(firebaseAuth).admin
+    const{email} = useContext(firebaseAuth)
     const{handleSignOut} = useContext(firebaseAuth)
+    const[fetchedCandidates,setFetchedCandidates]=useState([])
 
-    useEffect(async()=>{
-         const candidates= await firestoreMethods.fetchCandidates()
+    const Vote=()=>{
+
+    }
+
+    const fetchingUser=async()=>{
+        const list =[]
+        const candidates= await firestoreMethods.fetchCandidates()
          candidates.onSnapshot(documentSnapshot=>{
-             documentSnapshot.docs.map((doc)=>{
-                 console.log(doc.data())
+             const fetchCandidates=documentSnapshot.docs.map((doc)=>{
+                 console.log(doc.id)
+                 const id=doc.id
+                 const data=doc.data()
+                 return {
+                     data,
+                     id
+                 }
              })
+             
+             list.push([...fetchCandidates])
+             setFetchedCandidates([...fetchCandidates])
+             
          })
-         return ()=>{
-             candidates()
-         }
+         console.log(list)
+         
+    }
+
+    useEffect(()=>{
+         fetchingUser()
+         console.log(fetchedCandidates)
     },[])
+
+    
+
+    // console.log(candidates)
     return(
         <div>
           <UserHeaderBar email={email} handleSignOut={handleSignOut} />
-          <h3>WELCOME TO HOMEPAGE</h3>
+          <div style={{marginTop:10,width:600,margin:"0 auto"}}>
+              <List
+              itemLayout="horizontal"
+              size="large"
+              dataSource={fetchedCandidates}
+              renderItem={item=>(
+                  <div style={{
+                      display:"flex",
+                      flexDirection:"row",
+                      justifyContent:"space-between",
+                      alignItems:"center",
+                      marginBottom:50
+                  }}>
+                     <CandidateComponent
+                        uri={item.data?.imageDownloadUrl}
+                       candidateName={item.data?.candidateName}
+                       position={item.data?.candidatePostion}
+                        />
+
+                        <Button
+                        variant="contained"
+                        style={{backgroundColor:"green",color:"white"}}
+                        >
+                            <ThumbUpIcon style={{marginRight:5}} />
+                            Vote
+                        </Button>
+                  </div>
+
+              )}
+              >
+
+              </List>
+
+          </div>
+          
         </div>
     )
 }
